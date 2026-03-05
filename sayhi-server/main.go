43357c9fe1e774b8c4a4a599d309147917feae4e
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2026-03-05 12:02:25 +0700

    feat: basic server that send msg

---
+++ sayhi-server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net"
+	// "bufio"
+)
+
+var connList []net.Conn
+
+func handleConn(conn net.Conn) {
+	addr := conn.RemoteAddr()
+	log.Printf("connected to %s\n", addr)
+	conn.Write(append(append([]byte{0, 2}, []byte("BB")...), append([]byte{0, 5}, []byte("hallo")...)...))
+	// scanner := bufio.NewScanner(conn)
+	// for scanner.Scan() {
+	// 	log.Printf("%s says %s\n", addr, scanner.Text())
+	// }
+	conn.Close()
+	log.Printf("disconnected from %s\n", addr.String())
+}
+
+const PORT = "7777"
+
+func main() {
+	ln, err := net.Listen("tcp4", ":"+PORT)
+	if err != nil {
+		log.Fatalf("could not listen on port ':%s': %s\n", PORT, err)
+	}
+	log.Printf("listening for connection on port :%s\n", PORT)
+	connList = []net.Conn{}
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("could not accept a connection: %s\n", err)
+			continue
+		}
+		connList = append(connList, conn)
+		go handleConn(conn)
+	}
+}
