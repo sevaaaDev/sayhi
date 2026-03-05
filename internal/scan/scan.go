8588ab3a0d66f2b31cd844e01e0124ed268336ba
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2026-03-05 12:23:08 +0700

    feat: bufio split func for our msg protocol

---
+++ internal/scan/scan.go
@@ -0,0 +1,22 @@
+package scan
+
+import (
+	"encoding/binary"
+)
+
+func ScanMessage(data []byte, atEOF bool) (int, []byte, error) {
+	// if data is partial header and its already eof, just assume no token
+	if len(data) < 2 && atEOF {
+		return len(data), nil, nil
+	}
+	// if data is partial header, ask for more
+	if len(data) < 2 {
+		return 0, nil, nil
+	}
+	msgSize := int(binary.BigEndian.Uint16(data[0:2]))
+	// if msg in data is less than msgSize, ask for more data
+	if len(data) - 2 < msgSize {
+		return 0, nil, nil
+	}
+	return 2 + msgSize, data[2:msgSize+2:msgSize+2], nil
+}
